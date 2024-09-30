b6e8f1b5a09dabc47ed202dfd55a2393f92a6cc9
Kunal Dharpure <146354402+dharpurekunal08@users.noreply.github.com>
2024-09-30 12:49:58 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -2,10 +2,21 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-    
+var isValid = function (s) {
+    const stack = [];
+    const mapping = { ')': '(', '}': '{', ']': '[' };
+
+    for (let char of s) {
+        if (mapping[char]) { // Closing parenthesis
+            if (!stack.length || stack[stack.length - 1] !== mapping[char]) {
+                return false;
+            }
+            stack.pop();
+        } else { // Opening parenthesis
+            stack.push(char);
+        }
+    }
+
+    return !stack.length;
 };
-
-module.exports = { isValid };
-
 
