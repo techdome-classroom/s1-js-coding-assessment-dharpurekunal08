ee5293dcd60bbfc3a83beca9e47c1ea8d7cea6c1
Kunal Dharpure <146354402+dharpurekunal08@users.noreply.github.com>
2024-09-30 13:06:36 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,41 +1,45 @@
-const {isValid} = require("./program1");
-const assert = require("assert");
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    
+      
+        const matchingParentheses = {
+            ')': '(',
+            '}': '{',
+            ']': '['
+        };
+        
+       
+        const stack = [];
+        
+       
+        for (let char of s) {
+           
+            if (char in matchingParentheses) {
 
-describe("test cases for problem 1 ", function () {
+                const topElement = stack.length > 0 ? stack.pop() : '#';
+                
+               
+                if (matchingParentheses[char] !== topElement) {
+                    return false;
+                }
+            } else {
+                
+                stack.push(char);
+            }
+        }
+        
+        
+        return stack.length === 0;
+    }
+    
+   
+    console.log(isValid("()"));
+    console.log(isValid("()[]{}"));   
+    console.log(isValid("(]"));       
+    
 
-    it("test case 1", function () {
-        const result = isValid("()");
-        assert.equal(true, result);
-    });
 
-    it("test case 2", function () {
-        const result = isValid("()[]{}");
-        assert.equal(true, result);
-    });
-
-    it("test case 3", function () {
-        const result = isValid("{[()]}");
-        assert.equal(true, result);
-    });
-
-    it("test case 4", function () {
-        const result = isValid("(]");
-        assert.equal(false, result);
-    });
-
-    it("test case 5", function () {
-        const result = isValid("([)]");
-        assert.equal(false, result);
-    });
-
-    it("test case 6", function () {
-        const result = isValid("");
-        assert.equal(true, result);
-    });
-
-    it("test case 7", function () {
-        const result = isValid("(){");
-        assert.equal(false, result);
-    });
-
-});
+module.exports = { isValid };
